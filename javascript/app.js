93d34a55f0c990b7ff2a25a70461d1de735d5ec1
Kristin Helker <klhelker@gmail.com>
2020-01-21 21:07:38 -0600

    added buttons that contain the Giphs

--- javascript/app.js
+++ javascript/app.js
@@ -2,18 +2,6 @@
 var countries = [ "Argentina", "France", "Australia", "Mozambique", "Thailand", "India", "China", "bolivia", "Georgia", "Norway", "Russia", "Scotland"] 
 
 
-function displayCountryImage() {
- 
-  var country = $(this).attr("data-name");
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q="+country+"&api_key=6BDLXo72I7XzDjnAmW2Gmk5YusJyh70g&limit=10";
-  $.ajax({url: queryURL, method: "GET"}).then(function(response) {
-    for (var i = 0; i < response.data.length; i++){
-      $('#gifArea').prepend("<p>Rating: " + response.data[i].rating + "</p>");
-      $('#gifArea').prepend("<img src= '" + response.data[i].images.fixed_height_small.url + "'>");
-}
-  })
-
-}
 function renderButtons() {
 
     
@@ -30,73 +18,68 @@ function renderButtons() {
       a.text(countries[i]);
       // Adding the button to the HTML
       $("#buttons-view").append(a);
-    //   $('button').on('click',function (){
-    //     console.log("button clicked")
-    // }
+
   }
 }
 
-    $('add-country').on("click", function(event) {
+    $('#add-country').on("click", function(event) {
   
      event.preventDefault();
-    
-     $('button').on('click',function (){
 
-      
-      var x = $(this).data("search");
-      
-      var queryURL= "http://api.giphy.com/v1/gifs/search?q="+country+"&api_key=6BDLXo72I7XzDjnAmW2Gmk5YusJyh70g&limit=10";
-      
-      $.ajax({url: queryURL, method: "GET"}).done(function(response) {
-       console.log(response);
-       console.log(response.data[0].rating);
-       for (var i = 0; i < response.data.length; i++){
-               $('#gifArea').prepend("<p>Rating: " + response.data[i].rating + "</p>");
-               $('#gifArea').prepend("<img src= '" + response.data[i].images.fixed_height_small.url + "'>");
-               var newCountry = $("<button>");
-               var p = $("<p>").text("<p>Rating: " + response.data[i].rating + "</p>");
-               var countryImage = $("<img>");
-               countryImage.attr('src',response.data[i].images.fixed_height_small.url + "'>" );
-               newCountryDiv=append(p);
-               newCountryDiv=append(countryImage);
-              $('#gifArea').append(newCountryDiv);
+     var country=$("#country-input").val().trim();
 
-              }
-           
-           })
+     countries.push(country);
 
-           
-    //  var country = $("#country-input").val().trim();
-    // The country from the textbox is then added to our array
-    //  countries.push(country);
-
-    // calling renderButtons which handles the processing of our movie array
-    //  renderButtons();
-  })
-})
-
-
-  // Calling the renderButtons function at least once to display the initial list of movies
+     $("#buttons-view").empty();
      renderButtons();
 
 
-        
-  $("#add-country").on("click", function(event) {
-
-    event.preventDefault();
-
-    var country = $("#country-input").val();
-
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+country+"&api_key=6BDLXo72I7XzDjnAmW2Gmk5YusJyh70g&limit=10";
-
-    $.ajax({url: queryURL, method: "GET"}).done(function(response) {
-        console.log(response);
-        console.log(queryURL);
-        console.log(response.data[0].rating);
-        for (var i = 0; i < response.data.length; i++){
-                $('#gifArea').prepend("<p>Rating: " + response.data[i].rating + "</p>");
-                $('#gifArea').prepend("<img src= '" + response.data[i].images.fixed_height_small.url + "'>");
-        }
+    });
     
-                        })
-                      })
+     $("#buttons-view").on('click', ".country", function() {
+
+      var country = $(this).text().toLowerCase();
+
+      var queryURL= "http://api.giphy.com/v1/gifs/search?q="+country+"&api_key=6BDLXo72I7XzDjnAmW2Gmk5YusJyh70g&limit=10";
+
+      $.ajax({url: queryURL, method: "GET"}).then(function(response) {
+        
+        $("#gifArea").html("");
+        for(i = 0; i < response.data.length; i++){
+          
+          var animate = response.data[i].images.fixed_height_small.url;
+          var staticGif = response.data[i].images.fixed_height_small_still.url;
+          
+          console.log(response);
+       
+          var gifImage = $('<img>').attr({
+          
+               "src": staticGif,
+        "data-still": staticGif,
+        "data-animate": animate,
+        "data-state": "still",
+        "class": "gifImage",
+
+       })
+
+        $("#gifArea").prepend(gifImage);
+
+
+      //  for (var i = 0; i < response.data.length; i++){
+      //          $('#gifArea').prepend("<p>Rating: " + response.data[i].rating + "</p>");
+      //          $('#gifArea').prepend("<img src= '" + response.data[i].images.fixed_height_small.url + "'>");
+      //          var newCountry = $("<button>");
+      //          var p = $("<p>").text("<p>Rating: " + response.data[i].rating + "</p>");
+      //          countryImage.attr('src',response.data[i].images.fixed_height_small.url + "'>" );
+      //          newCountryDiv=append(p);
+      //          newCountryDiv=append(countryImage);
+      //         $('#gifArea').append(newCountryDiv);
+
+      }
+          })
+        })
+     $(document).ready(function(){
+       renderButtons();
+
+     })
+ 
